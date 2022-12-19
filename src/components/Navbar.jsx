94a1eace63e1cd6e658d2a8293fba8f79d8aadff
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-19 17:13:53 +0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,43 +21,45 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img
-          src={Logo}
-          alt="Logo Image"
-          style={{
-            width: "150px",
-            height: "70px",
-            objectFit: "cover",
-            borderRadius: "5px",
-            boxShadow: "2px 2px 5px black",
-            cursor: "pointer",
-          }}
-        />
+        <a href="/">
+          <img
+            src={Logo}
+            alt="Logo Image"
+            style={{
+              width: "150px",
+              height: "70px",
+              objectFit: "cover",
+              borderRadius: "5px",
+              boxShadow: "2px 2px 5px black",
+              cursor: "pointer",
+            }}
+          />
+        </a>
       </div>
 
       {/* menu */}
       <ul className="hidden md:flex">
-        <li className="hover:bg-sky-700 rounded-md">
+        <li className="hover:bg-sky-700 rounded-md hover:font-bold">
           <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="hover:bg-sky-700 rounded-md">
+        <li className="hover:bg-sky-700 rounded-md hover:font-bold">
           <Link to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className="hover:bg-sky-700 rounded-md">
+        <li className="hover:bg-sky-700 rounded-md hover:font-bold">
           <Link to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className="hover:bg-sky-700 rounded-md">
+        <li className="hover:bg-sky-700 rounded-md hover:font-bold">
           <Link to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className="hover:bg-sky-700 rounded-md">
+        <li className="hover:bg-sky-700 rounded-md hover:font-bold">
           <Link to="contact" smooth={true} duration={500}>
             Contact
           </Link>
@@ -77,30 +79,30 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl hover:bg-sky-700 rounded-md hover:font-bold">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl hover:bg-sky-700 rounded-md hover:font-bold">
           {" "}
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl hover:bg-sky-700 rounded-md hover:font-bold">
           {" "}
           <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl hover:bg-sky-700 rounded-md hover:font-bold">
           {" "}
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl hover:bg-sky-700 rounded-md hover:font-bold">
           {" "}
           <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
