21b2c8fcf2469d7a4e97c207e2278a2e14820d67
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-29 19:32:18 +0300

    typrescript,assets

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -35,7 +35,7 @@ const Skills = () => {
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={TypeScript} alt="HTML icon" />
-            <p className="my-4">JAVASCRIPT</p>
+            <p className="my-4">TYPESCRIPT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
