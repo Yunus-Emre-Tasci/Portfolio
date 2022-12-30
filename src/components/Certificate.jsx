7bd06d5be97d62cb7909202fe06db56f7aa47c69
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-30 09:49:22 +0300

    control

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -27,19 +27,23 @@ const Certificate = () => {
 
         <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-32 mx-auto" src={htmlcss} alt="HTML icon" />
+            <img className="w-28
+             mx-auto" src={htmlcss} alt="HTML icon" />
             <p className="my-4 text-lg font-bold">HTML-CSS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-32 mx-auto" src={javascript} alt="HTML icon" />
+            <img className="w-28
+             mx-auto" src={javascript} alt="HTML icon" />
             <p className="my-4 text-lg font-bold">JAVASCRIPT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-32 mx-auto" src={react} alt="HTML icon" />
+            <img className="w-28
+             mx-auto" src={react} alt="HTML icon" />
             <p className="my-4 text-lg font-bold">REACT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-32 mx-auto" src={frontend} alt="HTML icon" />
+            <img className="w-28
+             mx-auto" src={frontend} alt="HTML icon" />
             <p className="my-4 text-lg font-bold">FRONTEND</p>
           </div>
           {/* <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
